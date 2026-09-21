4b8543a6d1ff78d173400c02a489e13de557a342
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-09-21 23:54:16 +0300

    chore: refresh PWA cache for dock contract

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cnc-tolerance-full-v1.1.2-safari-dock';
+const CACHE='cnc-tolerance-full-v1.1.2-safari-dock-dockfix1';
 const ASSETS=['./','./index.html','./styles.css?v=1.1.2-safari-dock','./app.js?v=1.1.2-safari-dock','./manifest.webmanifest','./assets/icons/icon-180.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./assets/icons/icon-1024.png','./assets/icons/favicon-64.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
